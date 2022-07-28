1054250ca095ab96ce56400767157d177fd8c510
Arturo1603 <arturo.cq.16@gmail.com>
2022-07-28 15:11:40 -0500

    feact: footer

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from "./components/Footer";
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Footer/>
   </React.StrictMode>
 );
 
