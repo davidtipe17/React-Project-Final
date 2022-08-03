6e8503e4da7f2729dd7c2ce1abe32e168cf1fd39
Arturo1603 <arturo.cq.16@gmail.com>
2022-08-03 11:31:48 -0500

    nuevo

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,7 @@
 import {default as Chasupport} from "./Chasupport";
+import {default as CheckOutPayment} from "./CheckOutPayment";
+import {default as EditProfile} from "./EditProfile";
+import {default as FilterTecnicos} from "./FilterTecnicos";
+import {default as Footer} from "./Footer";
+import {default as FormAddTciket} from "./FormAddTicket";
+import {default as Login} from "./Login";
