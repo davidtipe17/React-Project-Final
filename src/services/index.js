f4066b40f45105e8790b315097214d8e43fab5bf
Arturo1603 <arturo.cq.16@gmail.com>
2022-08-10 18:40:53 -0500

    feat, user register, no validated

--- src/services/index.js
+++ src/services/index.js
@@ -20,3 +20,25 @@ export const getUser = async () => {
     return error;
   }
 };
+
+export const createUser = async (newUser) => {
+    try {
+        const response =await fetch(Url_User, {
+            method: "POST",
+            body: JSON.stringify({
+                name: newUser.name,
+                apellido: newUser.apellido,
+                celular: newUser.celular,
+                profesion: newUser.profesion,
+                correo: newUser.correo,
+                contrasena: newUser.contrasena,
+            }),
+            headers:{
+                "Content-type": "application/json",
+            },
+        });
+        return response.ok;  
+    } catch (error) {
+        return error
+    }
+}
